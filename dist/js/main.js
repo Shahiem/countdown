d65673e932cb3be8c152dd38c9ea906c2ca3137b
Shahiem Seymor <s.seymor@live.nl>
2018-04-04 20:06:49 +0200

    Update code for cleanup

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,51 +1,54 @@
 class Countdown {
     constructor(identifier) {
-        this.element = document.querySelector(identifier);
-        this.countSecond = 0;
+      this.element = document.querySelector(identifier);
+      this.intervalCounter = 0;
 
-        this.interval = setInterval(
-            this.draw.bind(this), 
-            1000
-        );
+      this.intervalMethod = setInterval(
+        this.drawTimer.bind(this), 
+        1000
+      );
     }
 
     addZero(number) {
-        return number  <= 9 ? '0' + number : number;
+      return number  <= 9 ? '0' + number : number;
     }
     
-    draw() {   
-        this.countSecond++;
+    drawTimer() {   
+      this.intervalCounter++;
 
-        let now = new Date();
+      let startDate = new Date();
         
-        let specificDay = new Date();
-        specificDay.setDate(10);
-        specificDay.setHours(18, 0, 0);
+      // Set spec
+      let endDate = new Date();
+      endDate.setDate(10);
+      endDate.setHours(18, 0, 0);
 
-        let days = specificDay.getDate() - now.getDate();
-        let diffrence = specificDay.getTime() - now.getTime();
-        
-        // Times
-        let hours = this.addZero(Math.floor(diffrence % 86400000 / 3600000));
-        let minutes = this.addZero(Math.floor(diffrence % 3600000 / 60000));
-        let seconds = this.addZero(Math.floor(diffrence % 60000 / 1000));
+      let daysLeft = endDate.getDate() - startDate.getDate();
 
-        switch(days) {
-            case 0:
-                this.element.textContent = 'Nog ' + hours + ':' + minutes + ':' + seconds;
-                break;
-            case 1:
-                this.element.textContent = 'Nog 1 dag en ' + hours + ':' + minutes + ':' + seconds;
-                break;
-            default:
-                this.element.textContent = 'Nog ' + days + ' dagen';
-        }
+      // Calculate time diffrence
+      let timeMs = endDate.getTime() - startDate.getTime();
+      let timeHours = this.addZero(Math.floor(timeMs % 86400000 / 3600000));
+      let timeMinutes = this.addZero(Math.floor(timeMs % 3600000 / 60000));
+      let timeSeconds = this.addZero(Math.floor(timeMs % 60000 / 1000));
 
-        if (this.countSecond >= 30) {
-            clearInterval(this.interval);
-        }
+      switch(daysLeft) {
+        case 0:
+          this.element.textContent = 'Nog ' + timeHours + ':' + timeMinutes + ':' + timeSeconds; // Day 10
+          break;
+        case 1:
+          this.element.textContent = 'Nog 1 dag en ' + timeHours + ':' + timeMinutes + ':' + timeSeconds; // Day 9
+          break;
+        default:
+          this.element.textContent = 'Nog ' + daysLeft + ' dagen'; // Day < 9
+      }
+
+      // Clear timer after 30 seconds
+      if (this.intervalCounter >= 30) {
+        clearInterval(this.intervalMethod);
+      }
     }
 }
+
 let clock = new Countdown('.countdown');
 
 // Instance 2
