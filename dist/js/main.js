e7bd4c2b61055c38fac776d414f1c1757d490275
Shahiem Seymor <s.seymor@live.nl>
2018-04-05 10:22:30 +0200

    Count to 6 pm only on the last day

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,9 +14,9 @@
       // Set date on 10th day of the current month
       this.endDate = new Date();
       this.endDate.setDate(10);
-      this.endDate.setHours(18, 0, 0);
+      this.endDate.setHours(0, 0, 0);
 
-      this.daysLeft = endDate.getDate() - startDate.getDate();
+      this.daysLeft = this.endDate.getDate() - this.startDate.getDate();
 
       this.intervalMethod = setInterval(
         this.drawTimer.bind(this), 
@@ -29,34 +29,36 @@
     }
     
     drawTimer() {   
-      this.intervalCounter++;
+      if (this.daysLeft <= 1) {
+        this.intervalCounter++;
 
-      // Calculate time diffrence
-      let timeMs = this.endDate.getTime() - this.startDate.getTime();
-      let timeHours = this.addZero(Math.floor(timeMs % 86400000 / 3600000));
-      let timeMinutes = this.addZero(Math.floor(timeMs % 3600000 / 60000));
-      let timeSeconds = this.addZero(Math.floor(timeMs % 60000 / 1000));
+        // Count to 6pm on the last day
+        if (this.daysLeft == 0) {
+          this.endDate.setHours(18, 0, 0);
+        }
 
-      switch(this.daysLeft) {
-        case 0:
+        // Calculate time diffrence
+        let timeMs = this.endDate.getTime() - this.startDate.getTime();
+        let timeHours = this.addZero(Math.floor(timeMs % 86400000 / 3600000));
+        let timeMinutes = this.addZero(Math.floor(timeMs % 3600000 / 60000));
+        let timeSeconds = this.addZero(Math.floor(timeMs % 60000 / 1000));
+
+        if (this.daysLeft == 0) {
           this.element.textContent = 'Nog ' + timeHours + ':' + timeMinutes + ':' + timeSeconds; // Day 10
-          break;
-        case 1:
-          this.element.textContent = 'Nog 1 dag en ' + timeHours + ':' + timeMinutes + ':' + timeSeconds; // Day 9
-          break;
-        default:
-          this.element.textContent = daysLeft > 0 ? 'Nog ' + this.daysLeft + ' dagen' : ''; // Day < 9
+        } else {
+          this.element.textContent = 'Nog 1 dag  en ' + timeHours + ':' + timeMinutes + ':' + timeSeconds; // Day 10
+        }
+
+        // Clear timer after 30 seconds
+        if (this.intervalCounter >= 30) 
+          clearInterval(this.intervalMethod);
+      } else {
+        this.element.textContent = this.daysLeft > 0 ? 'Nog ' + this.daysLeft + ' dagen' : ''; // Day < 9
       }
 
       // Empty text content when date has passed
-      if (this.startDate > this.endDate) {
+      if (this.startDate > this.endDate)
         this.element.textContent = '';
-      }
-
-      // Clear timer after 30 seconds
-      if (this.intervalCounter >= 30) {
-        clearInterval(this.intervalMethod);
-      }
     }
 }
 
